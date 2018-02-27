76a4a783d3e2abada2f59c6b473e671228a7bc62
Jasmine Gadie and Megan Lee <jasmine@>
2018-02-26 19:44:58 -0800

    updated variable names and labels in js and html, added to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var drinkInput = $("input#drink").val();
     var animalInput= $("input#animal").val();
     var colorInput = $("input#color").val();
-
+    var favoritethings = ["input#food", "input#drink", "input#animal", "input#color"]
 
     $(".food").text(foodInput);
     $(".drink").text(drinkInput);
